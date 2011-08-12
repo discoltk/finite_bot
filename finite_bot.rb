728ce8671ce9ac5d377efb34c59205f1911d8ce5
hMz <achemze@gmail.com>
2011-08-12 10:32:33 -0700

    integrated APP_CONFIG

--- finite_bot.rb
+++ finite_bot.rb
@@ -16,14 +16,16 @@ require './lib/finite_bot/tinyurl'
 
 include FiniteBot
 
+$APP_CONFIG = YAML.load_file("./config/irc.yaml")
+
 bot = Cinch::Bot.new do
   configure do |c|
-   c.server = "irc.efnet.net"
-   c.port = "6667"
-   c.nick = "fbot12"
-   c.user = "silicon"
-   c.password = ""
-   c.channels = ["#baykids"]
+   c.server          = $APP_CONFIG[:server]
+   c.port            = $APP_CONFIG[:port]
+   c.nick            = $APP_CONFIG[:nick]
+   c.user            = $APP_CONFIG[:user]
+   c.password        = $APP_CONFIG[:password]
+   c.channels        = $APP_CONFIG[:channels].to_array
    c.plugins.plugins = [TinyURL, ForEX, Imap]
    c.plugins.options[Imap] = {
      :host => '',
