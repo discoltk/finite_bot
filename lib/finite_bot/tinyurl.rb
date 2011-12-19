04709551932e36265895319151059379e497fde5
silicon <achemze@gmail.com>
2011-12-18 23:22:04 -0800

    added display_titles to tinyurl

--- lib/finite_bot/tinyurl.rb
+++ lib/finite_bot/tinyurl.rb
@@ -8,6 +8,12 @@ module FiniteBot
 
     listen_to :channel
 
+    def title(url)
+      html  = open(url).read
+      title = %r{<title>(.*)</title>}m.match(html)[1]
+      title.squeeze(" ").gsub("\r"," ").gsub("\n"," ").squeeze(" ").strip
+    end
+
     def shorten(url)
       url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
       url == "Error" ? nil : url
@@ -19,8 +25,14 @@ module FiniteBot
       urls = URI.extract(m.message, ["http", "https"])
 
       unless urls.first.nil?
-        unless urls.first.size < ($APP_CONFIG[:tinyurl][:minimum_characters] || 25)
-          short_urls = urls.map { |url| shorten(url) }.compact
+        unless urls.first.size < (APP_CONFIG[:tinyurl][:minimum_characters] || 25)
+          short_urls = urls.map { |url|
+            if APP_CONFIG[:tinyurl][:display_titles].nil? or APP_CONFIG[:tinyurl][:display_titles] == false
+              shorten(url)
+            else
+              shorten(url)+" -- "+title(url)
+            end
+          }.compact
           m.reply short_urls.join(", ") unless short_urls.empty?
         end
       end
