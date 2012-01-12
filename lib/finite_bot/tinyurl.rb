e298a03a7d8b6cc9ec8bfd230055d6cdf64e8151
silicon <achemze@gmail.com>
2012-01-12 00:36:49 -0800

    fixed bug with trying to display titles for images

--- lib/finite_bot/tinyurl.rb
+++ lib/finite_bot/tinyurl.rb
@@ -9,7 +9,11 @@ module FiniteBot
     listen_to :message
 
     def title(url)
-      html  = open(url).read
+      begin
+        html = open(url).read
+      rescue
+        return nil
+      end
       title = %r{<title>(.*)</title>}m.match(html)[1]
       title.squeeze(" ").gsub("\r"," ").gsub("\n"," ").squeeze(" ").strip
     end
@@ -26,14 +30,17 @@ module FiniteBot
 
       unless urls.first.nil?
         unless urls.first.size < (APP_CONFIG[:tinyurl][:minimum_characters] || 25)
-          short_urls = urls.map { |url|
-            if APP_CONFIG[:tinyurl][:display_titles].nil? or APP_CONFIG[:tinyurl][:display_titles] == false
-              shorten(url)
-            else
-              shorten(url)+" -- "+title(url)
+          unless APP_CONFIG[:tinyurl][:display_titles].nil? or APP_CONFIG[:tinyurl][:display_titles] == false
+            short_urls = urls.map { |url|
+              cur_title = title(url) ? title(url) : nil rescue nil
+
+              output = shorten(url)
+              output = output + " -- " + cur_title unless cur_title.nil?
+
+              output
+              }.compact
             end
-          }.compact
-          m.reply short_urls.join(", ") unless short_urls.empty?
+            m.reply short_urls.join(", ") unless short_urls.empty?
         end
       end
     end
